7ca06bc9c7ae854de1840e2fe546a53d7bfb9e76
Rodrigo Valentim de Mesquita <rodrigovalentimdemesquitaa@gmail.com>
2022-08-10 20:25:55 -0300

    aula 11 comentada
    só revisando

--- mynotes.php
+++ mynotes.php
@@ -1,24 +1,40 @@
 <!-- *** PAREI NA AULA 10 *** 
-------------------------------------------------------
+----------------------------------------------------------------
 * PHP
     - liguagem de programação para web
     - destina-se à criação de conteúdos dinâmicos    
-------------------------------------------------------
+----------------------------------------------------------------
 * tags
     >> <?php 
        ?>
-------------------------------------------------------
+----------------------------------------------------------------
 * Apresentar texto
     >> echo "texto"
     >> print "texto"
-------------------------------------------------------
+----------------------------------------------------------------
 * comentários
     >> // ou # == de uma linha
     >> /*  */ == vários linhas
-------------------------------------------------------
+----------------------------------------------------------------
+* Variável
+    - guardam dados
+    - podem ser modificadas
+    >> $examplevar
+----------------------------------------------------------------
+* Convenções
+    >> $umaVariavel == camel case(mais usado)
+    >> $uma_Variavel == snake case(mais facil de compreender)
+    >> $UmaVariavel == studli case(não recomendado)
+-----------------------------------------------------------------
+* Tipos de variáveis
+    >> $int = 100;         
+    >> $float = 10.5;           
+    >> $bool = true;             
+    >> $string = "olá mundo";
 
-
-
+    >> $array = [1,2,3]; == vários valores
+    >> $pessoa = new Pessoa(); == objeto com propriedade e métodos 
+    >> $nulo = null         
 
 
 
