94f431f6c749184e18f9e6e4df37a88a3e31ecc5
Rodrigo Valentim de Mesquita <rodrigovalentimdemesquitaa@gmail.com>
2022-08-11 06:47:34 -0300

    aula 18
    revisando e aprendendo detalhes não conhecidos

--- mynotes.php
+++ mynotes.php
@@ -43,6 +43,24 @@
 -------------------------------------------------------------------------
 * concatenação
     >> | . | .= |
+-------------------------------------------------------------------------
+* Caracteres de escape
+    >> echo '<p>Parágrafo de HTML dentro do PHP</p>';
+    - quando se tem algum dado que já possui aspas simples, devemos
+      usar aspas duplas para o código ao redor dele e virce e versa
+    - podemos ainda usar a barra invertida antes do apóstrofo
+    >> echo 'Nesse texto \' irá aparecer o apóstrofo\' do meio';
+-------------------------------------------------------------------------
+* Caracteres de escape unicode
+    >> echo "\u{A9}"; == copyright
+    >> "\u{BC"; == 1/4
+    - https://www.utf8-chartable.de/ == site dos unicodes
+-------------------------------------------------------------------------
+* HTML entites
+    >> echo '<br>';
+    >> echo '&copy';
+    - https://dev.w3.prg/html5/html-author/charref == site entites
+
 
 
 -->
